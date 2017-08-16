d92d03163e37fbd7d7be3ed5687e027ec6d40475
Zachary Williams <zlwilliams1@gmail.com>
2017-08-16 17:31:22 -0400

    Finished the CSS assignments

---
+++ WebFundamentals/Javascript/Basics/declare.js
@@ -0,0 +1,21 @@
+var neg = -123;
+var dec = 3.14159
+var pos = 42;
+var large = 987654321;
+var name = "Zachary Williams";
+var place = "Coding Dojo";
+var day = "Aug 16 2017";
+var isTrue = true;
+var isFalse = false;
+var invalid = undefined;
+
+console.log("negative number: " + neg);
+console.log("decimal number: " + dec);
+console.log("positive number: " + pos);
+console.log("big number: " + large);
+console.log("my name: " + name);
+console.log("current location: " + place);
+console.log("current date: " + day);
+console.log("a true boolean: " + isTrue);
+console.log("a false boolean: " + isFalse);
+console.log("invalid variable: " + invalid);
