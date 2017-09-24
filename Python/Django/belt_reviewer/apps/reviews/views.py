35a375d07b586edc05d1749022fe6db942a4a799
Zachary Williams <zlwilliams1@gmail.com>
2017-09-24 11:23:51 -0400

    Refactored belt reviewer a bit

--- Python/Django/belt_reviewer/apps/reviews/views.py
+++ Python/Django/belt_reviewer/apps/reviews/views.py
@@ -1,3 +1,4 @@
+# TODO: prettify, move all model logic to models.py, and deploy
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 import bcrypt
@@ -20,17 +21,15 @@ def register(request):
         return redirect("/books")
     if request.method == "POST":
         request.session['alias'] = request.POST['alias']
-        errors = User.objects.validator(request.POST)
+        register_result = User.objects.register(request.POST)
+        errors = register_result['errors']
         if len(errors):
             for tag, error in errors.iteritems():
-                messages.error(request, error, extra_tags=tag)
+                messages.error(request, error)
             request.session['name'] = request.POST['name']
             request.session['email'] = request.POST['email']
         else:
-            req = request.POST
-            hash1 = bcrypt.hashpw(req['password'].encode(), bcrypt.gensalt())
-            user = User.objects.create(name=req['name'], alias=req['alias'], email=req['email'], password=hash1)
-            request.session['user_id'] = user.id
+            request.session['user_id'] = register_result['user'].id
             return redirect("/books")
     return redirect("/")
 
@@ -39,15 +38,15 @@ def login(request):
     if request.session['user_id'] != "":
         return redirect("/books")
     if request.method == "POST":
-        errors = User.objects.login_validator(request.POST)
+        login_result = User.objects.login(request.POST)
+        errors = login_result['errors']
         if len(errors):
             for tag, error in errors.iteritems():
-                messages.error(request, error, extra_tags=tag)
+                messages.error(request, error)
             request.session['email'] = request.POST['email']
         else:
-            user = User.objects.get(email=request.POST['email'])
-            request.session['user_id'] = user.id
-            request.session['alias'] = user.alias
+            request.session['user_id'] = login_result['user'].id
+            request.session['alias'] = login_result['user'].alias
             return redirect("/books")
     return redirect("/")  
 
@@ -84,39 +83,22 @@ def new_book(request):
     if request.session['user_id'] == "":
         return redirect("/")  
     if request.method == "POST":  
-        authorErrors = Author.objects.validator(request.POST)
-        if len(authorErrors):
-            for tag, error in authorErrors.iteritems():
+        book = None
+        add_book_result = Book.objects.add_book(request.POST)
+        errors = add_book_result['errors']
+        if len(errors):
+            for tag, error in errors.iteritems():
                 messages.error(request, error, extra_tags=tag)
-                return redirect("/books/add")
+                return redirect("/books/add")       
+            request.session['title'] = request.POST['title']
+            request.session['review'] = request.POST['review']
+            request.session['stars'] = request.POST['stars']
         else:
-            bookErrors = Book.objects.validator(request.POST)
-            if len(bookErrors):
-                for tag, error in bookErrors.iteritems():
-                    messages.error(request, error, extra_tags=tag)
-                request.session['title'] = request.POST['title']
-                request.session['review'] = request.POST['review']
-                request.session['stars'] = request.POST['stars']
-                return redirect("/books/add")
-            else:
-                reviewErrors = Review.objects.validator(request.POST)
-                if len(reviewErrors):
-                    for tag, error in reviewErrors.iteritems():
-                        messages.error(request, error, extra_tags=tag)
-                    request.session['title'] = request.POST['title']
-                    request.session['review'] = request.POST['review']
-                    request.session['stars'] = request.POST['stars']
-                    return redirect("/books/add")
-                else:
-                    a = Author.objects.filter(name=request.POST['new_author'])
-                    if len(a) == 0:
-                        a = Author.objects.get(name=request.POST['existing_author'])
-                    else:
-                        a = Author.objects.get(name=request.POST['new_author'])
-                    book = Book.objects.create(title=request.POST['title'], author=a)
-                    user = User.objects.get(id=request.session['user_id'])
-                    review = Review.objects.create(text=request.POST['review'], stars=request.POST['stars'], book=book, reviewer = user)
-        return redirect("/books/" + str(book.id))
+            request.session['title'] = ""
+            request.session['review'] = ""
+            request.session['stars'] = "5"
+            return redirect("/books/" + str(add_book_result['book'].id))
+    return redirect('/books/add')
 
 def book(request, id):
     init_session(request)
@@ -163,17 +145,13 @@ def new_review(request, id):
     init_session(request)
     if request.session['user_id'] == "":
         return redirect("/")   
-    errors = Review.objects.validator(request.POST)
-    if len(errors):
+    errors = Review.objects.add_review(request.POST)
+    if len(errors): # if posting the new review went awry
         for tag, error in errors.iteritems():
             messages.error(request, error, extra_tags=tag)
         request.session['review'] = request.POST['review']
         request.session['stars'] = request.POST['stars']
-        return redirect("/books/" + id + "/new_review")
-    else:
-        user = User.objects.get(id=request.session['user_id'])
-        book = Book.objects.get(id=id)
-        Review.objects.create(text=request.POST['review'], stars=request.POST['stars'], reviewer=user, book=book)
+    return redirect("/books/" + id)
 
 def logout(request):
     reset_session(request)
@@ -193,15 +171,3 @@ def reset_session(request):
      request.session['title'] = ""
      request.session['new_author'] = ""
      request.session['stars'] = "3"
-
-    
-"""
-def alt_login(request):
-    result = User.manager.login(request.POST)
-    if not result[0]:
-        # get errors
-        for key, val in result[1].iteritems():
-            messages.error(request, val)
-        return redirect('/main')
-    return redirect()
-"""
