235cb26ad00088741ad9f2510fafbb9b41963bda
Zachary Williams <zlwilliams1@gmail.com>
2017-10-04 11:27:24 -0400

    Express survey form

--- MEAN/Express/counter/app.js
+++ MEAN/Express/counter/app.js
@@ -1,14 +1,12 @@
 var express = require("express");
 var session = require('express-session');
 var path = require("path");
-var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('views', path.join(__dirname, './views')); 
 app.set('view engine', 'ejs');
 app.use(session({secret: 'jfdlsaflksaj', resave: true, saveUninitialized: true}));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(request, response){
     if(request.session.count === undefined){
