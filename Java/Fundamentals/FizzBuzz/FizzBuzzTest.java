3ca831972675109d0758114b9e0d1da965dcdfeb
Zachary Williams <zlwilliams1@gmail.com>
2017-10-29 18:02:10 -0400

    FizzBuzz

---
+++ Java/Fundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz fb = new FizzBuzz();
+        System.out.println(fb.fizzBuzz(3));
+        System.out.println(fb.fizzBuzz(6));
+        System.out.println(fb.fizzBuzz(5));
+        System.out.println(fb.fizzBuzz(10));
+        System.out.println(fb.fizzBuzz(15));
+        System.out.println(fb.fizzBuzz(7));
+    }
+}
