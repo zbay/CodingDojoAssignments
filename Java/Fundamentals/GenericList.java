f4f9a5cbffe70a3ce015d1f899c4c2ca3c365f7e
Zachary Williams <zlwilliams1@gmail.com>
2017-10-30 11:35:19 -0400

    Exception handling

---
+++ Java/Fundamentals/GenericList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class GenericList{
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for(int i = 0; i < myList.size(); i++){
+            try{
+                Integer casted = (Integer) myList.get(i);
+            }
+            catch(ClassCastException cce){
+                System.out.println("Failed cast! " + myList.get(i));
+            }
+        }
+    }
+}
